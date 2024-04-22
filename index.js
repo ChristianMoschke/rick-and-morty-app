3b7aed0c30b27a269b86fbac4304f149edf2956d
Christian <christian.moschkeq@icloud.com>
2024-04-22 15:02:38 +0200

    add search bar feature

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+const searchBarInput = document.querySelector(".search-bar__input");
 
 let page = 1;
 let maxPage = 1;
+let searchQuery = ""; // State variable for search query
 
 async function fetchCharacters() {
   try {
     const response = await fetch(
-      `https://rickandmortyapi.com/api/character/?page=${page}`
+      `https://rickandmortyapi.com/api/character/?page=${page}&name=${searchQuery}`
     );
     const data = await response.json();
     const characters = data.results;
@@ -54,4 +56,11 @@ nextButton.addEventListener("click", () => {
     page++;
     fetchCharacters();
   }
+});
+
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchQuery = searchBarInput.value; // Update searchQuery with current text in search bar
+  page = 1; // Reset page to 1 when performing a new search
+  fetchCharacters(); // Fetch characters with updated search query
 });
